455f1a1c104b5ea2f5a2e12b2ddaebe84167df59
yassine-alaoui <alaoui6b@gmail.com>
2023-02-04 17:04:32 +0100

    book writting

---
+++ Assets/type_text.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class type_text : MonoBehaviour
+{
+    public TMP_Text text;
+    public string[] sentences;
+    public float delay = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    IEnumerator Type()
+    {
+        foreach (string sentence in sentences)
+        {
+            text.text = "";
+            foreach (char letter in sentence.ToCharArray())
+            {
+                text.text += letter;
+                yield return new WaitForSeconds(delay);
+            }
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
